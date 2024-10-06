0a14bb078aad3942d86b229b7c9eba051ec027dc
Lindsey Verbauwhede <mora376@hotmail.com>
2024-10-06 15:07:34 +0200

    first commit

---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,23 @@
+import Bottombar from "@/components/shared/BottomBar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/TopBar";
+import { Outlet } from "react-router-dom";
+
+
+
+const RootLayout = () => {
+  return (
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
+  );
+};
+
+export default RootLayout;
---
+++ src/components/shared/LeftSidebar.tsx
@@ -0,0 +1,106 @@
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+
+import { INavLink } from "@/types";
+import { sidebarLinks } from "@/constants";
+
+import { Button } from "@/components/ui/button";
+import { useSignOutAccount } from "@/lib/react-query/queries";
+import { useUserContext, INITIAL_USER } from "@/context/AuthContext";
+import Loader from "./Loader";
+
+const LeftSidebar = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { user, setUser, setIsAuthenticated, isLoading } = useUserContext();
+
+  const { mutate: signOut } = useSignOutAccount();
+
+  const handleSignOut = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    signOut();
+    setIsAuthenticated(false);
+    setUser(INITIAL_USER);
+    navigate("/sign-in");
+  };
+
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+          <div className="flex-center">
+            <img
+              src="/assets/images/logo.svg"
+              alt="logo"
+              className="w-12 h-12"
+            />
+            <h1>
+              <span className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500">
+                InstaPets
+              </span>
+            </h1>
+          </div>
+        </Link>
+
+        {isLoading || !user.email ? (
+          <div className="h-14">
+            <Loader />
+          </div>
+        ) : (
+          <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+            <img
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+              alt="profile"
+              className="h-14 w-14 rounded-full"
+            />
+            <div className="flex flex-col">
+              <p className="body-bold">{user.name}</p>
+              <p className="small-regular text-light-3">@{user.username}</p>
+            </div>
+          </Link>
+        )}
+
+        <ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: INavLink) => {
+            const isActive = pathname === link.route;
+
+            return (
+              <li
+                key={link.label}
+                className={`leftsidebar-link group ${
+                  isActive && "bg-primary-500"
+                }`}
+              >
+                <NavLink
+                  to={link.route}
+                  className="flex gap-4 items-center p-4"
+                >
+                  <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    className={`group-hover:invert-white ${
+                      isActive && "invert-white"
+                    }`}
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <Button
+        variant="ghost"
+        className="shad-button_ghost"
+        onClick={(e) => handleSignOut(e)}
+      >
+        <img src="/assets/icons/logout.svg" alt="logout" />
+        <p className="small-medium lg:base-medium">Logout</p>
+      </Button>
+    </nav>
+  );
+};
+
+export default LeftSidebar;
